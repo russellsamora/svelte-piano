a74eab596f08e3f6f41a78b967beb18527d9ae94
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-11 12:17:41 -0400

    prettier

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,41 +1,9 @@
 <script>
-  import Child from "./Child.svelte";
-  import wordmarkSvg from "../svg/wordmark.svg";
-  import petData from "../data/pets.csv";
+  import * as Tone from "tone";
+  // import Child from "./Child.svelte";
+  import pianoData from "../data/piano.csv";
 </script>
 
 <style>
-  /* vars defined in public/assets/global.css */
-  h1 {
-    color: var(--c1);
-  }
 
-  strong {
-    color: var(--c2);
-  }
-
-  img {
-    width: 20rem;
-    display: block;
-  }
 </style>
-
-<!-- <style src="../css/app.scss">
-
-</style> -->
-
-<a class="wordmark" href="https://pudding.cool">
-  {@html wordmarkSvg}
-</a>
-
-<h1>Hello!</h1>
-
-{#each petData as { name, pet }}
-  <p>
-    {name} has
-    <strong>{pet}.</strong>
-  </p>
-{/each}
-
-<img alt="smokey" src="assets/smokey.jpg" />
-<Child />
