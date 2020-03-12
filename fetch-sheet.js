ceed51b7168df4c244420c4766ad2e35a179edb2
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-12 14:42:24 -0400

    almost working

---
+++ fetch-sheet.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const request = require("request");
+const d3 = require("d3");
+
+const piano = d3.csvParse(fs.readFileSync("./src/data/piano.csv", "utf8"));
+const midiFull = piano.map(d => +d.midi).filter(d => d >= 36 && d <= 84);
+const durationFull = [0, 1, 2, 3, 4];
+
+function parseMidi({ value, sequence }) {
+  const midis = sequence.map(d => d.midi);
+  if (value === "exact") return [...new Set(midis)];
+  else if (value === "between") {
+    const [s, e] = d3.extent(midis);
+    return d3.range(s, e + 1).map(d => d);
+  } else if (value === "full") {
+    return midiFull;
+  }
+  // default to exact
+  return [...new Set(midis)];
+}
+
+function parseDuration({ value, sequence }) {
+  const durations = sequence.map(d => d.duration);
+  if (value === "exact") return [...new Set(durations)];
+  else if (value === "between") {
+    const [s, e] = d3.extent(durations);
+    return d3.range(s, e + 1).map(d => d);
+  } else if (value === "full") {
+    return durationFull;
+  }
+  // default to exact
+  return [...new Set(midis)];
+}
+
+function parseSequence(str) {
+  const notes = str.split(".");
+  return notes.map(d => {
+    const [midi, duration] = d.split("-").map(d => +d);
+    return { midi, duration };
+  });
+}
+
+function cleanData(data) {
+  return data
+    .filter(d => d.sequence)
+    .map(d => {
+      const sequence = parseSequence(d.sequence);
+      return {
+        title: d.title,
+        artist: d.artist,
+        odds: +d.odds,
+        apm: +d.apm,
+        est: +d.est,
+        tempo: +d.tempo,
+        sequence,
+        range: {
+          midis: parseMidi({ value: d.range_midi, sequence }),
+          durations: parseDuration({ value: d.range_duration, sequence })
+        }
+      };
+    });
+}
+
+function getSheet() {
+  const id = "1KOLaqf34-8e0S1VsMzIa6IafaTiCc0UE46UK6Owex7o";
+  const gid = 0;
+  const base = "https://docs.google.com/spreadsheets/u/1/d";
+  const url = `${base}/${id}/export?format=csv&id=${id}&gid=${gid}`;
+
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      else if (response && response.statusCode === 200) {
+        try {
+          const data = d3.csvParse(body);
+          const levels = cleanData(data);
+          fs.writeFileSync("./src/data/levels.json", JSON.stringify(levels));
+        } catch (err) {
+          reject(err);
+        }
+      } else reject(response.statusCode);
+    });
+  });
+}
+
+async function init() {
+  try {
+    await getSheet();
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+init();
--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,108 @@
+<style>
+  .key p {
+    margin: 0;
+    font-family: monospace;
+    padding: 0.125em;
+    line-height: 1;
+    text-align: center;
+    font-size: 11px;
+    font-weight: bold;
+  }
+  .key {
+    border: 1px solid #000;
+    width: 3rem;
+    cursor: pointer;
+    display: block;
+  }
+  .sharp {
+    background-color: #333;
+    color: #fff;
+  }
+</style>
+
 <script>
-  import * as Tone from "tone";
-  // import Child from "./Child.svelte";
+  import { onMount } from "svelte";
+  import midiToNotation from "./midi-to-notation.js";
   import pianoData from "../data/piano.csv";
+  import levels from "../data/levels.json";
+
+  let Tone;
+  let synth;
+  let sampler;
+
+  let selected;
+  let melody;
+
+  onMount(async () => {
+    const module = await import("tone");
+    Tone = module.default;
+    synth = new Tone.Synth().toMaster();
+    sampler = new Tone.Sampler(
+      {
+        C2: "C2.mp3",
+        "D#2": "Ds2.mp3",
+        "F#2": "Fs2.mp3",
+        A3: "A3.mp3",
+        C3: "C3.mp3",
+        "D#3": "Ds3.mp3",
+        "F#3": "Fs3.mp3",
+        A4: "A4.mp3",
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3",
+        C5: "C5.mp3",
+        "D#5": "Ds5.mp3",
+        "F#5": "Fs5.mp3",
+        A5: "A5.mp3",
+        C5: "C5.mp3"
+      },
+      {
+        release: 1,
+        baseUrl: "assets/notes/"
+      }
+    ).toMaster();
+  });
+
+  $: level = levels.find(d => d.title === selected);
+  $: if (Tone && level) {
+    Tone.Transport.stop();
+    if (melody) melody.dispose();
+    const values = midiToNotation(level.sequence);
+    melody = new Tone.Part((time, value) => {
+      sampler.triggerAttackRelease(value.note, value.duration, time);
+    }, values).start(0);
+
+    melody.humanize = true;
+
+    //set the transport
+    Tone.Transport.bpm.value = level.tempo;
+    Tone.Transport.start();
+  }
+
+  const lowC = 36;
+  const highC = 84;
+  let piano = pianoData.filter(d => +d.midi >= lowC && +d.midi <= highC);
+
+  function click(note, octave) {
+    // synth.triggerAttackRelease(`${note}${octave}`, "16n");
+    sampler.triggerAttackRelease(`${note}${octave}`, "16n");
+  }
 </script>
 
-<style>
-
-</style>
+<select bind:value="{selected}">
+  <option value="">Choose a Song</option>
+  {#each levels as l}
+    <option value="{l.title}">{l.title} by {l.artist}</option>
+  {/each}
+</select>
+<div class="piano">
+  {#each piano.reverse() as { midi, note, octave }}
+    <button
+      class="key"
+      class:sharp="{note.includes('#')}"
+      on:click="{() => play(note, octave)}">
+      {note}{octave}
+    </button>
+  {/each}
+</div>
