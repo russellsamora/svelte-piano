cd0e77107b53fd62b1cf51b3a49e32ecb61dd626
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-13 15:49:11 -0400

    better hmr

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -28,6 +28,7 @@
   import midiToNotation from "./midi-to-notation.js";
   import pianoData from "../data/piano.csv";
   import levels from "../data/levels.json";
+  import samples from "../data/samples.json";
 
   pianoData.reverse();
 
@@ -44,34 +45,14 @@
 
   onMount(async () => {
     const module = await import("tone");
-    Tone = module.default;
-    // synth = new Tone.Synth().toMaster();
-    sampler = new Tone.Sampler(
-      {
-        C2: "C2.mp3",
-        "D#2": "Ds2.mp3",
-        "F#2": "Fs2.mp3",
-        A3: "A3.mp3",
-        C3: "C3.mp3",
-        "D#3": "Ds3.mp3",
-        "F#3": "Fs3.mp3",
-        A4: "A4.mp3",
-        C4: "C4.mp3",
-        "D#4": "Ds4.mp3",
-        "F#4": "Fs4.mp3",
-        A4: "A4.mp3",
-        C5: "C5.mp3",
-        "D#5": "Ds5.mp3",
-        "F#5": "Fs5.mp3",
-        A5: "A5.mp3",
-        C5: "C5.mp3"
-      },
-      {
+    if (!Tone) {
+      Tone = module.default;
+      sampler = new Tone.Sampler(samples, {
         release: 0.5,
         baseUrl: "assets/notes/"
-      }
-    ).toMaster();
-    part = new Tone.Part(() => {}, []);
+      }).toMaster();
+      part = new Tone.Part(() => {}, []);
+    }
   });
 
   $: level = levels.find(d => d.title === selected);
@@ -83,7 +64,6 @@
       sampler.triggerAttackRelease(value.note, value.duration, time);
     }, values).start(0);
 
-    //set the transport
     Tone.Transport.bpm.value = level.tempo;
     Tone.Transport.timeSignature = level.sig || 4;
     Tone.Transport.start();
